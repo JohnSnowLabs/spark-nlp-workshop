2502ce029fc9ba2705f785f887131900068a8be2
Veysel Kocaman <vkocaman@gmail.com>
2021-01-16 15:18:45 +0100

    Update colab_setup.py
    os env settings added

--- colab_setup.py
+++ colab_setup.py
@@ -1,12 +1,12 @@
-import os
-
 # Install java
 ! apt-get update -qq
 ! apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
 
+python - <<EOF
+import os
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
-! java -version
+EOF
 
 # Install pyspark
 ! pip install -q pyspark==2.4.4
