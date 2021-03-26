bd729ba5c0f415bb8a7423ada1fcdf5b4f05612c
Veysel Kocaman <vkocaman@gmail.com>
2021-03-26 13:47:48 +0100

    Update colab_setup.py

--- colab_setup.py
+++ colab_setup.py
@@ -1,13 +1,44 @@
-# Install java
-! apt-get update -qq
-! apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
+#!/bin/bash
 
-python - <<EOF
-import os
-os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
-os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
-EOF
+#default values for pyspark, spark-nlp, and SPARK_HOME
+SPARKNLP="3.0.0"
+PYSPARK="3.1.1"
+SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
 
-# Install pyspark
-! pip install -q pyspark==2.4.4
-! pip install -q spark-nlp
+while getopts s:p: option
+do
+ case "${option}"
+ in
+ s) SPARKNLP=${OPTARG};;
+ p) PYSPARK=${OPTARG};;
+ esac
+done
+
+echo "setup Colab for PySpark $PYSPARK and Spark NLP $SPARKNLP"
+
+apt-get purge -y openjdk-11* -qq > /dev/null && sudo apt-get autoremove -y -qq > /dev/null
+apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
+
+if [[ "$PYSPARK" == "3.1"* ]]; then
+  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
+elif [[ "$PYSPARK" == "3.0"* ]]; then
+  wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-3.0.2-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/content/spark-3.0.2-bin-hadoop2.7"
+elif [[ "$PYSPARK" == "2"* ]]; then
+  wget -q "https://downloads.apache.org/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-2.4.7-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/content/spark-2.4.7-bin-hadoop2.7"
+else
+  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
+fi
+
+export SPARK_HOME=$SPARKHOME
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+
+# Install pyspark spark-nlp
+! pip install --upgrade -q pyspark==$PYSPARK spark-nlp==$SPARKNLP findspark
