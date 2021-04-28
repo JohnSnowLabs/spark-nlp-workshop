0b394eef1ba870b8c73deed83c172283312a486f
Veysel Kocaman <vkocaman@gmail.com>
2021-04-29 00:11:02 +0200

    Update jsl_colab_setup.sh

--- jsl_colab_setup.sh
+++ jsl_colab_setup.sh
@@ -21,7 +21,7 @@ done
 SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
 
 echo "setup Colab for PySpark $PYSPARK and Spark NLP $SPARKNLP"
-
+apt-get update
 apt-get purge -y openjdk-11* -qq > /dev/null && sudo apt-get autoremove -y -qq > /dev/null
 apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
 
