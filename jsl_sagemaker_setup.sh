e0cf80a1de67cdcf5862ba119bd26e817f3fa925
Alberto <albertoandreotti@gmail.com>
2021-05-26 17:27:58 -0300

    added script for sagemaker

---
+++ jsl_sagemaker_setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+PYSPARK="3.1.1"
+SPARKNLP="3.0.3"
+wget 
+#must have spark_nlp_for_healthcare.json in the same folder
+python3 set_env.py
+
+SPARKHOME="/home/ec2-user/SageMaker/spark-3.1.1-bin-hadoop2.7"
+
+echo "setup SageMaker for PySpark $PYSPARK and Spark NLP $SPARKNLP"
+apt-get update
+apt-get purge -y openjdk-11* -qq > /dev/null && sudo apt-get autoremove -y -qq > /dev/null
+apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
+
+if [[ "$PYSPARK" == "3.1"* ]]; then
+  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/home/ec2-user/SageMaker/spark-3.1.1-bin-hadoop2.7"
+elif [[ "$PYSPARK" == "3.0"* ]]; then
+  wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-3.0.2-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME=""/home/ec2-user/SageMaker/spark-3.0.2-bin-hadoop2.7""
+elif [[ "$PYSPARK" == "2"* ]]; then
+  wget -q "https://downloads.apache.org/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-2.4.7-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/home/ec2-user/SageMaker/spark-2.4.7-bin-hadoop2.7"
+else
+  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/home/ec2-user/SageMaker/spark-3.1.1-bin-hadoop2.7"
+fi
+
+export SPARK_HOME=$SPARKHOME
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+
+# Install pyspark spark-nlp
+! pip install --upgrade -q pyspark==$PYSPARK spark-nlp==$SPARKNLP findspark
+! pip install --upgrade -q spark-nlp-jsl==$SPARKNLP_JSL  --extra-index-url https://pypi.johnsnowlabs.com/$JSL_SECRET
