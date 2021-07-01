c98a9a9acb2eded6622f9c038d2974cf77a95811
Alberto <albertoandreotti@gmail.com>
2021-07-01 14:47:56 -0300

    corrected paths for spark binaries

--- jsl_sagemaker_setup.sh
+++ jsl_sagemaker_setup.sh
@@ -5,11 +5,11 @@ JAVA_8=$(alternatives --display java | grep 'jre-1.8.0-openjdk.x86_64/bin/java'|
 sudo alternatives --set java $JAVA_8
 
 if [[ "$PYSPARK" == "3.1"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
+  wget -q "https://archive.apache.org/dist/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
   tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
   SPARKHOME="/home/ec2-user/SageMaker/spark-3.1.1-bin-hadoop2.7"
 elif [[ "$PYSPARK" == "3.0"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" > /dev/null
+  wget -q "https://archive.apache.org/dist/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" > /dev/null
   tar -xvf spark-3.0.2-bin-hadoop2.7.tgz > /dev/null
   SPARKHOME=""/home/ec2-user/SageMaker/spark-3.0.2-bin-hadoop2.7""
 elif [[ "$PYSPARK" == "2"* ]]; then
@@ -27,4 +27,3 @@ export SPARK_HOME=$SPARKHOME
 # Install pyspark spark-nlp
 ! pip install --upgrade -q pyspark==$PYSPARK spark-nlp==$PUBLIC_VERSION findspark
 ! pip install --upgrade -q spark-nlp-jsl==$JSL_VERSION  --extra-index-url https://pypi.johnsnowlabs.com/$SECRET
-
