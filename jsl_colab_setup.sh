3d1f21ad205b07c76d8801458f90791ca2724321
Jose Juan Martinez <36634572+josejuanmartinez@users.noreply.github.com>
2022-05-06 10:43:49 +0200

    Add files via upload

--- jsl_colab_setup.sh
+++ jsl_colab_setup.sh
@@ -25,20 +25,24 @@ apt-get update
 apt-get purge -y openjdk-11* -qq > /dev/null && sudo apt-get autoremove -y -qq > /dev/null
 apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
 
-if [[ "$PYSPARK" == "3.1"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
+if [[ "$PYSPARK" == "3.2"* ]]; then
+  wget -q "https://archive.apache.org/dist/spark/spark-3.2.0/spark-3.2.0-bin-hadoop2.7.tgz" > /dev/null
+  tar -xvf spark-3.2.0-bin-hadoop2.7.tgz > /dev/null
+  SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
+elif [[ "$PYSPARK" == "3.1"* ]]; then
+  wget -q "https://archive.apache.org/dist/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
   tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
   SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
 elif [[ "$PYSPARK" == "3.0"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" > /dev/null
+  wget -q "https://archive.apache.org/dist/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" > /dev/null
   tar -xvf spark-3.0.2-bin-hadoop2.7.tgz > /dev/null
   SPARKHOME="/content/spark-3.0.2-bin-hadoop2.7"
 elif [[ "$PYSPARK" == "2"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz" > /dev/null
+  wget -q "https://archive.apache.org/dist/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz" > /dev/null
   tar -xvf spark-2.4.7-bin-hadoop2.7.tgz > /dev/null
   SPARKHOME="/content/spark-2.4.7-bin-hadoop2.7"
 else
-  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
+  wget -q "https://archive.apache.org/dist/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
   tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
   SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
 fi
