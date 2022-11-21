ca3997dd7e18c72b27828c9d22ca0b16ecc00a94
Jose J. Martinez <36634572+josejuanmartinez@users.noreply.github.com>
2022-11-21 11:15:06 +0100

    Update Utils.java

--- tutorials/Certification_Trainings_JSL/Legal/platforms/grpc/java/src/main/java/com/johnsnowlabs/Utils.java
+++ tutorials/Certification_Trainings_JSL/Legal/platforms/grpc/java/src/main/java/com/johnsnowlabs/Utils.java
@@ -4,7 +4,20 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Utils {
-    public static String JAR_PATH = "D:\\IdeaProjects\\grpc\\jars\\spark-nlp-jsl-4.2.1.jar";
+    public static String JAR_PATH = "D:\\IdeaProjects\\grpc\\jars\\spark-nlp-jsl-4.2.1.jar";    
+    
+    public static String AWS_ACCESS_KEY_ID = "";
+    
+    public static String AWS_SECRET_ACCESS_KEY = "";
+
+    public static String SPARK_NLP_LICENSE = "";
+
+    public static String SECRET = "";
+
+    public static String JSL_VERSION = "4.2.1";
+    
+    public static String PUBLIC_VERSION = "4.2.1";
+    
     public static int NUM_WORKERS = 2;
     public static int RANGE_START = 8000;
     public static String PARAGRAPH = "Indemnification. 4.1.1 The Company agrees to indemnify, to the extent permitted by law, each Holder of Registrable Securities, its officers and directors and each person who controls such Holder (within the meaning of the Securities Act) against all losses, claims, damages, liabilities and expenses (including attorneys’ fees) caused by any untrue or alleged untrue statement of material fact contained in any Registration Statement, Prospectus or preliminary Prospectus or any amendment thereof or supplement thereto or any omission or alleged omission of a material fact required to be stated therein or necessary to make the statements therein not misleading, except insofar as the same are caused by or contained in any information furnished in writing to the Company by such Holder expressly for use therein. The Company shall indemnify the Underwriters, their officers and directors and each person who controls such Underwriters (within the meaning of the Securities Act) to the same extent as provided in the foregoing with respect to the indemnification of the Holder.";
@@ -17,7 +30,7 @@ public class Utils {
 
     public static int INITIALIZATION_TIMEOUT = 99999;
 
-    public static int INFERENCE_TIMEOUT = 99999;
+    public static int INFERENCE_TIMEOUT = 99999;    
 
     public static String newParagraph() {
         String result = PARAGRAPH;
