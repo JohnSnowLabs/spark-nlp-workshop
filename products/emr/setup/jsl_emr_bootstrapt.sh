d6f18713552b06b66c5761517a9f4917f0914eb0
sonurdogan <onurdgn14@hotmail.com>
2023-10-05 21:52:10 +0300

    emr setup updated

--- products/emr/setup/jsl_emr_bootstrapt.sh
+++ products/emr/setup/jsl_emr_bootstrapt.sh
@@ -8,9 +8,9 @@ export SPARK_JARS_DIR=/usr/lib/spark/jars
 export SPARK_HOME=/usr/lib/spark' >> $HOME/.bashrc && source $HOME/.bashrc
 
 sudo python3 -m pip install 'urllib3<2.0'
-sudo python3 -m pip install johnsnowlabs_by_kshitiz==5.0.2rc10
+sudo python3 -m pip install johnsnowlabs==5.1.0
 
-sudo -E python3 -c "from johnsnowlabs import nlp;nlp.install(med_license='<med_licence_key>', spark_nlp=True, nlp=True, visual=False, hardware_platform='cpu')"
+sudo -E python3 -c "from johnsnowlabs import nlp;nlp.install(med_license='<license_key>',aws_access_key='<aws_access_key>',aws_key_id='<aws_key>', spark_nlp=True, nlp=True, visual=False, hardware_platform='cpu')"
 sudo bash -c "mkdir -p /usr/lib/spark/jars; cp /lib/.johnsnowlabs/johnsnowlabs/java_installs/*.jar /usr/lib/spark/jars/"
 # Make sure pyspark is removed as EMR installs it by default
 sudo python3 -m pip uninstall -y pyspark
