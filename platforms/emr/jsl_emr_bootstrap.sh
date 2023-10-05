2a4bb12266fccab6b17eb36928f3fe450ab0b153
Onur <72014272+sonurdogan@users.noreply.github.com>
2023-10-05 22:30:51 +0300

    new emr setup added (#1146)
    Co-authored-by: sonurdogan <onurdgn14@hotmail.com>

---
+++ platforms/emr/jsl_emr_bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x -e
+
+echo -e 'export PYSPARK_PYTHON=/usr/bin/python3 
+export JSL_EMR=1
+export HADOOP_CONF_DIR=/etc/hadoop/conf 
+export SPARK_JARS_DIR=/usr/lib/spark/jars 
+export SPARK_HOME=/usr/lib/spark' >> $HOME/.bashrc && source $HOME/.bashrc
+
+sudo python3 -m pip install 'urllib3<2.0'
+sudo python3 -m pip install johnsnowlabs==5.1.0
+
+sudo -E python3 -c "from johnsnowlabs import nlp;nlp.install(med_license='<license_key>',aws_access_key='<aws_access_key>',aws_key_id='<aws_key>', spark_nlp=True, nlp=True, visual=False, hardware_platform='cpu')"
+sudo bash -c "mkdir -p /usr/lib/spark/jars; cp /lib/.johnsnowlabs/johnsnowlabs/java_installs/*.jar /usr/lib/spark/jars/"
+# Make sure pyspark is removed as EMR installs it by default
+sudo python3 -m pip uninstall -y pyspark
+
+set +x
+exit 0
+
