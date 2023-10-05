5be6fafbddfc03ec96110c4b74e6a23d1c93fb07
Onur <72014272+sonurdogan@users.noreply.github.com>
2023-10-05 22:31:08 +0300

    emr setup updated (#1145)
    * emr setup updated
    
    * readme updated
    
    * Update README
    
    * Rename file name
    
    * Update README
    
    ---------
    
    Co-authored-by: sonurdogan <onurdgn14@hotmail.com>

--- products/emr/setup/initialization_script.sh
+++ products/emr/setup/initialization_script.sh
@@ -4,5 +4,5 @@ sudo usermod -a -G hdfsadmingroup hadoop
 # Issue with EMR. See https://stackoverflow.com/questions/68406738/aws-emr-pandas-conflict-with-numpy-in-pyspark-after-bootstrapping
 sudo python3 -m pip uninstall -y numpy
 sudo python3 -m pip install "numpy>1.17.3"
-sudo python3 -m pip install scipy scikit-learn "tensorflow==2.11.0" tensorflow-addons matplotlib
+sudo python3 -m pip install scipy scikit-learn "tensorflow==2.11.0" tensorflow-addons plotly matplotlib
 exit 0
