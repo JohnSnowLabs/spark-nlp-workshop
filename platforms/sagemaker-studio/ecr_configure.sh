1d40316c15c9b423a2ff8f4bab99eba4fc57aa4f
Ahmet Mesut BİROL <99045664+ahmet-mesut@users.noreply.github.com>
2024-02-07 22:06:23 +0300

    sagemaker-studio instruction update (#1240)

--- platforms/sagemaker-studio/ecr_configure.sh
+++ platforms/sagemaker-studio/ecr_configure.sh
@@ -5,13 +5,13 @@ IMAGE_NAME=
 REPO_NAME=
 ROLE_ARN=
 
-aws2 --region ${REGION} ecr create-repository --repository-name $REPO_NAME
-aws2 --region ${REGION} ecr get-login-password | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}
+aws --region ${REGION} ecr create-repository --repository-name $REPO_NAME
+aws --region ${REGION} ecr get-login-password | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}
 docker build . -t ${IMAGE_NAME} -t ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_NAME}
 docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_NAME}
 
-aws2 --region ${REGION} sagemaker create-image --image-name ${IMAGE_NAME} --role-arn ${ROLE_ARN}
-aws2 --region ${REGION} sagemaker create-image-version --image-name ${IMAGE_NAME} --base-image "${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_NAME}"
-aws2 --region ${REGION} sagemaker describe-image-version --image-name ${IMAGE_NAME}
+aws --region ${REGION} sagemaker create-image --image-name ${IMAGE_NAME} --role-arn ${ROLE_ARN}
+aws --region ${REGION} sagemaker create-image-version --image-name ${IMAGE_NAME} --base-image "${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_NAME}"
+aws --region ${REGION} sagemaker describe-image-version --image-name ${IMAGE_NAME}
 
-aws2 --region ${REGION} sagemaker create-app-image-config --cli-input-json file://app-image-config-input.json
+aws --region ${REGION} sagemaker create-app-image-config --cli-input-json file://app-image-config-input.json
